68ae979e130edbc32de7616f331698429f272a24
Saurav Kumar <54290238+saurav806@users.noreply.github.com>
2023-07-04 22:07:16 +0530

    basic

---
+++ Graph/Basic- Shortest path from 1 to n.cpp
@@ -0,0 +1,22 @@
+//Link:- https://practice.geeksforgeeks.org/problems/shortest-path-from-1-to-n0156/1
+
+//User function Template for C++
+
+class Solution{   
+public:
+    int minimumStep(int n){
+        //complete the function here
+        int count=0;
+        while(n!=1){
+            if(n%3==0){
+                count++;
+                n/=3;
+            }
+            else{
+                count++;
+                n-=1;
+            }
+        }
+        return count;
+    }
+};
