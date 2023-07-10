12a23715dcf7c19fdab1f72599a74cac74f11495
Saurav Kumar <54290238+saurav806@users.noreply.github.com>
2023-07-10 20:30:05 +0530

    Fibonacci

---
+++ Dynamic Programming/Easy- 509. Fibonacci Number.cpp
@@ -0,0 +1,18 @@
+//Link:- https://leetcode.com/problems/fibonacci-number/
+
+class Solution {
+public:
+    int count(int n,vector<int>&dp){
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        if(dp[n]!=-1)
+            return dp[n];
+        return dp[n]=count(n-1,dp)+count(n-2,dp);
+    }
+    int fib(int n) {
+        vector<int>dp(n+1,-1);
+        return count(n,dp);
+    }
+};
