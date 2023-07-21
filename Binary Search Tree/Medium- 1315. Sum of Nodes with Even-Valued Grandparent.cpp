791b90b9397ce21a8bf807b16077af319bedc7b6
Saurav Kumar <54290238+saurav806@users.noreply.github.com>
2023-07-21 11:38:04 +0530

    Sum Even-Valued Grandparent

--- Binary Search Tree/Medium- 1315. Sum of Nodes with Even-Valued Grandparent.cpp
+++ Binary Search Tree/Medium- 1315. Sum of Nodes with Even-Valued Grandparent.cpp
@@ -21,3 +21,40 @@ public:
         return sum;
     }
 };
+
+
+//Approach 2. BFS
+
+class Solution {
+public:
+    int sumOf(queue<pair<TreeNode*,TreeNode*>>&parent,int &sum){
+        while(parent.size()!=0){
+            int n=parent.size();
+            while(n-- > 0){
+                TreeNode * gParent=parent.front().second;
+                TreeNode * baap = parent.front().first;
+                parent.pop();
+                    if(baap->left){
+                        if(gParent->val %2 == 0)
+                            sum+=(baap->left->val);
+                        parent.push({baap->left,baap});
+                    }
+                    if(baap->right){
+                        if(gParent->val %2 == 0)
+                            sum+=(baap->right->val);
+                        parent.push({baap->right,baap});
+                    }
+            }
+        }
+        return sum;
+    }
+    int sumEvenGrandparent(TreeNode* root) {
+        queue<pair<TreeNode*, TreeNode*>>parent;
+        if(root->right)
+        parent.push({root->right,root});
+        if(root->left)
+        parent.push({root->left,root});
+        int sum=0;
+        return sumOf(parent,sum);;
+    }
+};
