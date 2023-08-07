c84c416a3215e672fffcb4b11f9173f74452d957
Saurav Kumar <54290238+saurav806@users.noreply.github.com>
2023-08-07 23:36:19 +0530

    Sudoku

---
+++ Recursion & Backtracking/Medium- Sudoku Solver.cpp
@@ -0,0 +1,46 @@
+//Link:- https://practice.geeksforgeeks.org/problems/solve-the-sudoku-1587115621/1
+class Solution 
+{
+    public:
+    //Function to find a solved Sudoku. 
+    bool isSafe(int row, int col, int grid[N][N], int val){
+        for(int i=0;i<9;i++){
+            if(grid[row][i] == val) return false;
+            if(grid[i][col] == val) return false;
+            if(grid[3*(row/3) + i/3][3*(col/3) + i%3] == val)
+                return false;
+        }
+        return true;
+    }
+    bool SolveSudoku(int grid[N][N])  
+    { 
+        // Your code here
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                if(grid[i][j] == 0){
+                    for(int val=1;val<=9;val++){
+                        if(isSafe(i,j,grid,val)){
+                            grid[i][j] = val;
+                            bool temp= SolveSudoku(grid);
+                            if(temp) return true;
+                            else
+                                grid[i][j] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    //Function to print grids of the Sudoku.
+    void printGrid (int grid[N][N]) 
+    {
+        // Your code here 
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++)
+                cout<<grid[i][j]<<" ";
+        }
+    }
+};
